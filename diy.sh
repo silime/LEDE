09fa0e28ff20b5cf1a542a1f53eb3be271f75370
silime <60662669+silime@users.noreply.github.com>
2020-05-21 01:01:38 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,7 +2,7 @@
 sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3.5 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib linux-libc-dev
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 
 #sed -i 's/OpenWrt/Silime/g' package/base-files/files/bin/config_generate
 
@@ -34,6 +34,8 @@ git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/mento
 #git clone https://github.com/Lienol/openwrt-package.git  package/openwrt-package
 git clone https://github.com/silime/luci-app-kodexplorer.git package/luci-app-kodexplorer
 git clone https://github.com/silime/luci-app-mentohust.git package/luci-app-mentohust
+git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+git clone https://github.com/kenzok8/small package/small
 #git clone https://github.com/rufengsuixing/luci-app-syncdial.git package/luci-app-syncdial
 #./scripts/feeds update luci-lib-jsonc
 #./scripts/feeds install luci-lib-jsonc
